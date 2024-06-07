a403b506968501101103c4cc09afa9f206b0d8aa
Kareena Patel <kareenapat17@gmail.com>
2024-06-07 14:19:58 +0100

    Added validation and interactivity to when request order btn is pressed

--- assets/js/quantityBtn.js
+++ assets/js/quantityBtn.js
@@ -2,6 +2,10 @@ let increaseQuantityBtn = document.getElementById("quantity-btn-increase");
 let decreaseQuantityBtn = document.getElementById("quantity-btn-decrease");
 let productQuantity = document.getElementById("quantity__input-box");
 
+let addToCartBtn = document.getElementById("add-to-cart-btn");
+
+
+
 increaseQuantityBtn.addEventListener("click", () => {
   productQuantity.value = parseInt(productQuantity.value) + 1;
 
@@ -14,4 +18,14 @@ decreaseQuantityBtn.addEventListener("click", () => {
 
   // In case if user presses button with no value in input
   productQuantity.value === "NaN" ? productQuantity.value = 1 : productQuantity.value
+})
+
+
+addToCartBtn.addEventListener("click", () => {
+  if (parseInt(productQuantity.value) && parseInt(productQuantity.value) > 0) {
+    alert("Your order has been requested")
+  }
+  else {
+    alert("An invalid quantity has been entered - enter a valid value")
+  }
 })
